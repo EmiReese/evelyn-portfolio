2b6a9f9f27e0b7c8aa302dc44b91d7e9ee54baf9
emily <emilyreese.ga@gmail.com>
2024-01-23 11:42:36 -0500

    added portfolio section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,10 +5,6 @@ const Header = () => {
   return (
     <header>
       <h2>Evelyn Walsh</h2>
-      <div className="header__socials">
-        <a href="https://www.linkedin.com/in/emily-reese-55488629a/"><FaLinkedin /></a>
-        <a href="https://github.com/EmiReese"><FaGithub /></a>
-      </div>
     </header>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import "./portfolio.css"
-// import image1 from "../../assets/image1.jpg"
-// import image3 from "../../assets/image3.jpg"
-// import image2 from "../../assets/image2.jpg"
+import image1 from "../../assets/image1.png"
+import image2 from "../../assets/image2.png"
+import image3 from "../../assets/image3.png"
+import image4 from "../../assets/image4.jpg"
+import image5 from "../../assets/image5.jpg"
+import MSU from "../../assets/MSU.pdf"
+import culinaryArts from "../../assets/culinaryArts.pdf"
+import adultCare from "../../assets/adultCare.pdf"
+import AShelterFromTheStorm from "../../assets/AShelterFromTheStorm.pdf"
+import Healthcare from "../../assets/Healthcare.pdf"
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -13,31 +20,56 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
-            {/* <img src={image1} alt="CSS coding" /> */}
+            <img src={image1} alt="CSS coding" />
           </div>
-          <h3>Professional Work from FetchGoat</h3>
+          <h3>Northwestern & Care@Network</h3>
           <div className='portfolio__item-cta'>
-            <a href="https://github.com/EmiReese/FetchGoat" className='btn' target="_blank">Github</a>
+          <a href={adultCare} className='
+        btn' download>Download PDF</a>
           </div>
         </article>
 
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
-            {/* <img src={image3} alt="apple laptop" /> */}
+            <img src={image2} alt="apple laptop" />
           </div>
-          <h3>TypeScript Application</h3>
+          <h3>Spartans Win with Care.com</h3>
           <div className='portfolio__item-cta'>
-            <a href="https://emireese.github.io/EvolveFitness/" className='btn' target="_blank">Github</a>
+            <a href={MSU} className='
+        btn' download>Download PDF</a>
           </div>
         </article>
 
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
-            {/* <img src={image2} alt="javascript coding" /> */}
+            <img src={image3} alt="javascript coding" />
           </div>
-          <h3>C# Number Guesser</h3>
+          <h3>The Culinary Arts Thrive in Cobb</h3>
           <div className='portfolio__item-cta'>
-            <a href="https://github.com/EmiReese/NumberGuesser" className='btn' target="_blank">Github</a>
+          <a href={culinaryArts} className='
+        btn' download>Download PDF</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={image4} alt="apple laptop" />
+          </div>
+          <h3>A Shelter from the Storm</h3>
+          <div className='portfolio__item-cta'>
+            <a href={AShelterFromTheStorm} className='
+        btn' download>Download PDF</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={image5} alt="javascript coding" />
+          </div>
+          <h3>Fighting Adult Obesity</h3>
+          <div className='portfolio__item-cta'>
+          <a href={Healthcare} className='
+        btn' download>Download PDF</a>
           </div>
         </article>
       </div>
