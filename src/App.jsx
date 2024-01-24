3b708b761fc0ae8e2383ecfabd36c0090bfda2a7
emily <emilyreese.ga@gmail.com>
2024-01-24 11:55:59 -0500

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import About from "./components/about/About"
 import Portfolio from "./components/portfolio/Portfolio";
 import Footer from './components/footer/Footer';
-import Header from "./components/header/Header"
+import Header from "./components/header/Header";
+import Contact from "./components/contact/Contact";
 const App = () => {
   return (
     <>
     <Header/>
     <About/>
     <Portfolio />
+    <Contact />
     <Footer />
     </>
   )
